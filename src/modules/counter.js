f56a1e37f2efe68b1be8fad81c831986ede2384d
jgam <jgam@alumni.nd.edu>
2020-03-28 16:03:00 +0900

    loggerMiddleware done

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -8,7 +8,8 @@ export const decrease = createAction(DECREASE);
 
 const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
 
-const counter = handleActions(
+//creating reducer called counter
+const counter = handleActions(//reducer, defaultState--so state.counter = 0
   {
     [INCREASE]: state => state + 1,
     [DECREASE]: state => state - 1
