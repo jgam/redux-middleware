cc028e1360d4a6f2bb170ff30baeb39a2c80cf68
jgam <jgam@alumni.nd.edu>
2020-03-28 16:36:19 +0900

    thunk of async

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -6,9 +6,21 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
+export const increaseAsync = () => dispatch => {
+    setTimeout(() => {
+      dispatch(increase());
+    }, 1000);
+  };
+  export const decreaseAsync = () => dispatch => {
+    setTimeout(() => {
+      dispatch(decrease());
+    }, 1000);
+  };
+  
 const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
 
-//creating reducer called counter
+//creating reducer called counter(object)
 const counter = handleActions(//reducer, defaultState--so state.counter = 0
   {
     [INCREASE]: state => state + 1,
